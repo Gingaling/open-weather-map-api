390cced8c1220b208728599b757ae972c2483f3f
Gingaling <nycgingsf@hotmail.com>
2022-02-20 18:45:35 -0800

    Committing files.

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const url = `api.openweathermap.org/data/2.5/weather?zip={${input.val}},us&units=imperial&appid={df35e93312fb4ec3f7593d0a28cae1aa}`;
+// const url = `api.openweathermap.org/data/2.5/weather?zip=${document.querySelector(<input type='submit'.val)},us&units=imperial&appid={df35e93312fb4ec3f7593d0a28cae1aa}`;
 
-$(button.submit).on('click', () => {
-	fetch(url)
-	    .then(function (response) { 
-			return response.json;
+{/* document.querySelector('input').on('submit', performFetch(event)); */}
+
+	fetch(https://api.openweathermap.org/data/2.5/weather?zip=94109&units=imperial&appid=df35e93312fb4ec3f7593d0a28cae1aa)
+		.then(res)(() => {
+			console.log('success!', res);
+			return res.json;
 		})
-		.then get (data.json){
-			console.log(products = json);
-			initialize(products);
-		})
-		.catch(function () +> {
-			console.log('Fetch problem: ' + err.message);
-		})
-	};
+		.catch((err) => {
+			console.log('something went wrong...', err);
+		});
 }
